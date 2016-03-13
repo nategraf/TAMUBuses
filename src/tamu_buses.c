ff835df62338ca743d818e3b8deef3ae9b00b1ad
Victor "Nate" Graf <nategraf@verizon.net>
2016-03-13 18:51:05 -0500

    The starting blocks
    Mostly template code as a test for the appmessage interface. Sends a message back and forth from the Pebble to the phone on button press

---
+++ src/tamu_buses.c
@@ -0,0 +1,80 @@
+#include <pebble.h>
+
+static Window *s_window;	
+	
+// Keys for AppMessage Dictionary
+// These should correspond to the values you defined in appinfo.json/Settings
+enum {
+	STATUS_KEY = 0,	
+	MESSAGE_KEY = 1
+};
+
+// Write message to buffer & send
+static void send_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_cstring(iter, MESSAGE_KEY, "I'm a Pebble!");
+	
+	dict_write_end(iter);
+  app_message_outbox_send();
+}
+
+void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send_message();
+}
+
+void config_provider(Window *window) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, down_single_click_handler);
+}
+
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *tuple;
+	
+	tuple = dict_find(received, STATUS_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
+	}
+	
+	tuple = dict_find(received, MESSAGE_KEY);
+	if(tuple) {
+		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
+	}
+}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {	
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+}
+
+static void init(void) {
+	s_window = window_create();
+	window_stack_push(s_window, true);
+	
+  window_set_click_config_provider(s_window, (ClickConfigProvider) config_provider);
+  
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+
+  // Initialize AppMessage inbox and outbox buffers with a suitable size
+  int inbox_size = app_message_inbox_size_maximum();
+  int outbox_size = app_message_outbox_size_maximum();
+	app_message_open(inbox_size, outbox_size);
+}
+
+static void deinit(void) {
+	app_message_deregister_callbacks();
+	window_destroy(s_window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
